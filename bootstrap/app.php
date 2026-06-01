ae61305babbb4e7fa96c8a10ff3e0dafa18e2797
Ironor25 <rock.on0103@gmail.com>
2026-06-01 19:28:58 +0530

    fixed code

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
-        'verify.user' => \App\Http\Middleware\VerifyUser::class,
-    ]);
+            'verify.user' => \App\Http\Middleware\VerifyUser::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
